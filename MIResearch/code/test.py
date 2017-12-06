e1e308fd8acb04acee5df24af14dd88148f6f6d1
matthew <guitarbass95@gmail.com>
2017-12-05 23:52:34 -0500

    got that data processed and into ssed and into convenient tsvs

--- MIResearch/code/test.py
+++ MIResearch/code/test.py
@@ -1,40 +1,66 @@
 from processData import processData as p
 import os
 
+
+REMOVE_STOP_WORDS = True
+
+opd = [ "train_pos.tsv",
+        "train_neg.tsv",
+        "train_unlab.tsv",
+        "test_pos.tsv",
+        "test_neg.tsv"]
+
+
 def process():
 
     #fn = os.path.join(os.path.dirname(__file__), 'my_file')
 
     train = "data/aclImdb/train/"
-
-
     test = "data/aclImdb/test/"
+    #outputdirs
 
 
     #process training data 0.pos 1.neg, 2.unsup
     trpfp, trnfp, trufp = p.loadRaw(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", train), True)
 
-    os.rename(trpfp.name, "train_pos.tsv")
-    os.rename(trnfp.name, "train_neg.tsv")
-    os.rename(trufp.name, "train_unlab.tsv")
+    os.rename(trpfp.name,opd[0])
+    os.rename(trnfp.name,opd[1])
+    os.rename(trufp.name,opd[2])
 
     #process test data 0.pos, 1.neg
-    tp, tn = p.loadRaw(os.path.join(os.path.dirname(__file__),"..",  train))
+    tp, tn = p.loadRaw(os.path.join(os.path.dirname(__file__),"..",  test))
 
-    os.rename(tp.name, "test_pos.tsv")
-    os.rename(tn.name, "test_neg.tsv")
+    os.rename(tp.name,opd[3])
+    os.rename(tn.name,opd[4])
+
+    filePointers = [trpfp, trnfp, trufp, tp, tn ]
+    return filePointers, opd
+
+def clean(fnList):
+    print("gonna clean!")
+
+    reviews = {}
 
 
+    for fn in fnList:
+        review = p.importTSV(fn)
+        clean_review, clean_review_dataframe  = p.cleanData(review, REMOVE_STOP_WORDS)
+        clean_review_dataframe.to_csv(("clean_" + fn), sep='\t', index=False)
+        file_name = os.path.splitext(fn)[0]
+        reviews.update({file_name : clean_review})
 
-def clean():
+    print("cleaned!")
 
-    print("no cleaning yet!")
-    return 0
+    return reviews
 
 
 
 def main():
-    process()
-    clean()
+    filePointers = process()
+
+    reviews = clean(opd)
+    for key, value in reviews.items():
+        print(key)
+    print(reviews["train_pos"][0])
 
 main()
